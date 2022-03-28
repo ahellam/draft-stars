aff8001264d4fa2a618d0b9294045d9367a3f903
Aaron Hellam <aaronhellam@gmail.com>
2022-03-28 15:13:52 -0700

    setting up router-dom

---
+++ src/DraftPlayers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DraftPlayers() {
+  return (
+    <div className="draftPlayers">
+        <h2>Draft Your Players</h2>
+    </div>
+  )
+}
+
+export default DraftPlayers
