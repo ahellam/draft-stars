23193009990add833fdb53e1fba79a854423e9b0
Aaron Hellam <aaronhellam@gmail.com>
2022-03-31 13:38:30 -0700

    links active

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <nav>
         <div className="Links">
-          <Link exact to="/">HOME</Link>
-          <Link to="/draft">DRAFT</Link>
-          <Link to="/create">CREATE</Link>
+          <NavLink exact to="/" activeClassName="activeLink">HOME</NavLink>
+          <NavLink to="/draft" activeClassName="activeLink">DRAFT</NavLink>
+          <NavLink to="/create" activeClassName="activeLink">CREATE</NavLink>
         </div>
       </nav>
       <h1 className="DraftStars">DRAFT STARS</h1>
