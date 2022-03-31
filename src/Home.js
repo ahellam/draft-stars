b5edbb5533f0f32fd1c5e775f16ee5265549a5db
Aaron Hellam <aaronhellam@gmail.com>
2022-03-31 12:29:28 -0700

    home done

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import {React} from 'react'
+import { useHistory } from "react-router-dom";
+import './home.css';
 
 function Home() {
+
+  const history = useHistory()
+
+  function linkToDraft () {
+    history.push("/draft")
+  }
   return (
     <div className="home">
-        <h1>Welcome to Draft Stars Home Page!</h1>
+
+        <h1 className="Header1">🏈 &nbsp;&nbsp; Welcome to Draft Stars &nbsp;&nbsp; 🏈</h1>
+    
+        <p className="Rules">
+          At Draft Stars, we believe football is more exciting with skin in the game.<br></br>
+          For that reason, we are committed to responsibly creating the worlds favorite betting experience.<br></br>
+          Configure your team by drafting an all star quarterback and an explosive wide receiever.<br></br> 
+          The higher the players rank, the more expensive the player is, so keep an eye on your budget!<br></br>
+          If you are unsatisfied with the available players, you may choose to create your own.<br></br>
+          Please bet responsibly!
+        </p>
+
+        <div className="GetStarted">
+          <button className="GoToDraft" onClick={linkToDraft}> GET STARTED </button>
+        </div>
+
+
     </div>
   )
 }
